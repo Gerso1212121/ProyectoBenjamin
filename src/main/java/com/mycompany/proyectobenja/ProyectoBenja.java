9aeb9b2ec4076ab9190e6ffc85e960b298ea5947
franc <franc@192.168.101.3>
2024-10-13 21:26:54 -0600

    Primer Commitholsssssss

---
+++ src/main/java/com/mycompany/proyectobenja/ProyectoBenja.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectobenja;
+
+/**
+ *
+ * @author franc
+ */
+public class ProyectoBenja {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
